3864b292f4de380efca28d73369c8cadd17b9ef7
Shahid Khan <107818138+ShahidHKhan@users.noreply.github.com>
2026-03-10 10:33:28 -0400

    extra

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Pea Tea "Physical Therapy""</h1>
+      <h1 class="title">Pea Tea "Physical Therapy"</h1>
       <p>Starter page. Add your content here.</p>
     </div>
   </section>
